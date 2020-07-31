3af8887dc255ef5ddbb4a897c1c17806b65cefbe
lidongling <asd.6735089>
2020-07-31 11:15:14 +0800

    测试提交

--- src/main/java/com/atguigu/springboot/config/SpringXunHuanYiLaiConfig.java
+++ src/main/java/com/atguigu/springboot/config/SpringXunHuanYiLaiConfig.java
@@ -6,5 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = {"com.atguigu.springboot.entity"})
 public class SpringXunHuanYiLaiConfig {
-
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
 }
